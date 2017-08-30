c93b080a72e3ff3fbef209387ddc6cf4c024baa0
Nico Villegas <nikodtb@gmail.com>
2017-08-30 18:47:37 -0500

    Se hace refactor de codigo para estandarizar, se agrega problema extra con valor inicial distinto

--- broken-calculator.py
+++ broken-calculator.py
@@ -1,6 +1,6 @@
-#Implementa una clase para resolver el problema de "Broken Calculator"
-import time
-
+#Implement a class to resolve the problem called "Broken Calculator".
+#Author: A01746886 Vazquez Nava Roberto Carlos 
+#		 A01746887 Villegas Felix Nicolas 
 
 from search import ( # Bases para construcción de problemas
     Problem, Node, Graph, UndirectedGraph,
@@ -16,127 +16,132 @@ from search import ( # Algoritmos de búsqueda informada (heurística)
     recursive_best_first_search, astar_search
 )
 
+import time
 
-MULTIPLICACION = 1 
-SUMA = 2
-RESTA = 3
+#Define CONSTANTS
+MULTIPLICATION = 1 
+SUM = 2
+SUBTRACTION = 3
 DIVISION = 4	 
-CONCATENACION = 5
+CONCATENATION = 5
 RESET = 6
 
 class BrokenCalculator(Problem):
-	def __init__(self,initial=0, values=[2,3],goal=20,acciones=['MUL']):
+	def __init__(self,initial=0, values=[2,3],goal=20,array_actions=['MUL']):
 			Problem.__init__(self,initial,goal)
-			#~ self.level = level  #Level for the exercise}
-			self.actualValue = 0
-			self.valuesusing = values
-			self.count = len(self.valuesusing)
-			self.acciones = acciones
-			#~ self.acciones = ['MUL','SUM','MINUS','DIV','CC']
+			self.using_values = values
+			self.count = len(self.using_values)
+			self.array_actions = array_actions
 
 	def actions(self,state):
-		acciones = []
-		for i in self.valuesusing:
-			for accion in self.acciones:
-				if accion == 'MUL':
-					acciones.append('MUL')
-				elif accion == 'SUM':
-					acciones.append('SUM')
-				elif accion == 'MINUS':
-					acciones.append('MINUS')
-				elif accion == 'DIV':
-					acciones.append('DIV')
-				elif accion == 'CC':
-					acciones.append('CC')
-				elif accion == 'AC':
-					acciones.append('AC')
-		return acciones				
+		actions_return = []
+		for i in self.using_values:
+			for action in self.array_actions:
+				if action == 'MUL':
+					actions_return.append('MUL')
+				elif action == 'SUM':
+					actions_return.append('SUM')
+				elif action == 'MINUS':
+					actions_return.append('MINUS')
+				elif action == 'DIV':
+					actions_return.append('DIV')
+				elif action == 'CC':
+					actions_return.append('CC')
+				elif action == 'AC':
+					actions_return.append('AC')
+		return actions_return				
 
-	def result(self, estado, accion):		
-		"El resultado se calcula sumando el estado anterior con el nuevo estado."
-		#~ newvalue = randint(2,3)
-		newvalue = self.valuesusing[self.count-1]
-		if accion == self.acciones[len(self.acciones)-1]:	
+	def result(self, state, action):		
+		"The result is calculated by adding the previous state to the new state."
+		new_value_state = self.using_values[self.count-1]
+		if action == self.array_actions[len(self.array_actions)-1]:	
 			self.count -= 1
 			if self.count == 0:
-				self.count = len(self.valuesusing)
+				self.count = len(self.using_values)			
+		if action == 'MUL':
+			return get_new_state(state,new_value_state,MULTIPLICATION)   
+		elif action == 'SUM':
+			return get_new_state(state,new_value_state,SUM) 
+		elif action== 'MINUS':
+			return get_new_state(state,new_value_state,SUBTRACTION)  
+		elif action == 'DIV':
+			return get_new_state(state,new_value_state,DIVISION)
+		elif action == 'CC':
+			return  get_new_state(state,new_value_state,CONCATENATION)
+		elif action == 'AC':
+			return  get_new_state(state,new_value_state,RESET)
 			
-		if accion == 'MUL':
-			#~ print(accion+str(newvalue))
-			return nuevo_estado(estado,newvalue,MULTIPLICACION)   
-		elif accion == 'SUM':
-			#~ print(accion+str(newvalue))
-			return nuevo_estado(estado,newvalue,SUMA)  #donde 2 es sumar
-		elif accion== 'MINUS':
-			#~ print(accion+str(newvalue))
-			return nuevo_estado(estado,newvalue,RESTA)  
-		elif accion == 'DIV':
-			#~ print(accion+str(newvalue))
-			return nuevo_estado(estado,newvalue,DIVISION)
-		elif accion == 'CC':
-			return  nuevo_estado(estado,newvalue,CONCATENACION)
-		elif accion == 'AC':
-			return  nuevo_estado(estado,newvalue,RESET)
-			
+	#Heuristic		
 	def h(self, node):
-		hn = node.state
-		gl = self.goal
-		return abs(gl-hn)
+		return abs(self.goal-node.state)
 
 
-def nuevo_estado(estado,nuevovalor,operacion):
-	nuevoestado = 0
-	if operacion == MULTIPLICACION:
-		nuevoestado = estado*nuevovalor
-		#~ print(nuevoestado)
-		return nuevoestado
-	elif operacion == SUMA:
-		nuevoestado = estado+nuevovalor
-		#~ print(nuevoestado)
-		return nuevoestado
-	elif operacion == RESTA:
-		nuevoestado = estado-nuevovalor
-		#~ print(nuevoestado)
-		return nuevoestado
-	elif operacion == DIVISION:
-		nuevoestado = estado/nuevovalor
-		#~ print(nuevoestado)
-		return nuevoestado
-	elif operacion == CONCATENACION:
-		nuevoestado = float(str(estado)+str(nuevovalor))
-		#~ print(nuevoestado)
-		return nuevoestado
-	elif operacion == RESET:
-		nuevoestado = 0
-		#~ print(nuevoestado)
-		return nuevoestado
+def get_new_state(state,new_value,operation):
+	new_state = 0
+	if operation == MULTIPLICATION:
+		new_state = state*new_value
+		return new_state
+	elif operation == SUM:
+		new_state = state+new_value
+		return new_state
+	elif operation == SUBTRACTION:
+		new_state = state-new_value
+		return new_state
+	elif operation == DIVISION:
+		new_state = state/new_value
+		return new_state
+	elif operation == CONCATENATION:
+		new_state = float(str(state)+str(new_value))
+		return new_state
+	elif operation == RESET:
+		new_state = 0
+		return new_state
 
-def despliega_solucion(nodo_meta):
-	 actiones = nodo_meta.solution()
-	 nodos = nodo_meta.path()
-	 print("inicial: "+str(nodos[0].state))
-	 for na in range(len(actiones)):
-	 	print(actiones[na]+": "+str(nodos[na+1].state))
+def display_solution(goal_node):
+	 actions_solution = goal_node.solution()
+	 nodes = goal_node.path()
+	 print("Initial: "+str(nodes[0].state))
+	 for i in range(len(actions_solution)):
+	 	print(actions_solution[i]+": "+str(nodes[i+1].state))
 
-#['MUL','SUM','MINUS','DIV','CC']
-prob1 = BrokenCalculator(0,[3],10002,['MUL','SUM','MINUS','DIV'])
+#['MUL','SUM','MINUS','DIV','CC','AC']
+prob1 = BrokenCalculator(0,[3,9,10],60,['MUL','SUM','MINUS','DIV'])
 #---------------------------------------------------------------------------------------------------#
 #-----------------------SOLUCION PROBLEMA-----------------------------------------------------------#
 #---------------------------------------------------------------------------------------------------#
-ini = time.time()
-#~ meta1 = breadth_first_search(prob1)
-#~ if meta1:
-	#~ despliega_solucion(meta1)
-	#~ print("tiempo empleado metodo 1: "+str(time.time()-ini))
-#~ else:
-	#~ print("Falla")
-ini = time.time()	
-meta2 = astar_search(prob1)
-
-if meta2:
-	print("tiempo empleado metodo 2: "+str(time.time()-ini))
-	despliega_solucion(meta2)
+initial_time = time.time()
+goal1 = breadth_first_search(prob1)
+if goal1:
+	display_solution(goal1)
+	print("Time to method 1 Breadth First: "+str(time.time()-initial_time))
 else:
-	print("Falla")
-	
+	print("Fail")
+initial_time = time.time()	
+goal2 = astar_search(prob1)
 
+if goal2:
+	display_solution(goal2)
+	print("Time to method 2 A*: "+str(time.time()-initial_time))
+else:
+	print("Fail")
+	
+prob2 = BrokenCalculator(5,[2,4,7],100,['MUL','SUM','MINUS','AC'])
+#---------------------------------------------------------------------------------------------------#
+#-----------------------SOLUCION PROBLEMA-----------------------------------------------------------#
+#---------------------------------------------------------------------------------------------------#
+initial_time = time.time()
+goal3 = breadth_first_search(prob2)
+if goal3:
+	display_solution(goal3)
+	print("Time to method 1 Breadth First: "+str(time.time()-initial_time))
+else:
+	print("Fail")
+initial_time = time.time()	
+goal4 = astar_search(prob2)
+
+if goal4:
+	display_solution(goal4)
+	print("Time to method 2 A*: "+str(time.time()-initial_time))
+else:
+	print("Fail")
+	
